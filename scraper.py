d7e3d9f271832c3357666d37a8cb97e6ea9d4e3a
zephyrxvxx7 <zephyrxvxx7@gmail.com>
2018-05-22 11:56:18 +0800

    First upload.

---
+++ scraper.py
@@ -0,0 +1,116 @@
+import json
+import requests
+from time import sleep
+from bs4 import BeautifulSoup
+from bs4.element import NavigableString
+import re
+
+class ptt_scraper():
+    main_url = "https://www.ptt.cc/bbs/"
+    root_url = "https://www.ptt.cc"
+    over18_payload = {
+        'from': 'bbs/Gossiping/index.html', 
+        'yes': 'yes'
+    }
+
+    def __init__(self):
+        self.session = requests.session()
+        requests.packages.urllib3.disable_warnings()
+        self.session.post("https://www.ptt.cc/ask/over18",
+                          verify=False,
+                          data=self.over18_payload)
+
+    def scraper(self, board="Gossiping", start=38900, end=38900, sleep_time = 0.5):
+        scraper_range = range(start, end + 1)
+
+        for page in self._pages(board, int(start), int(end)):
+            res = []
+            print('parse page url:', page)
+            for article in self._articles(page):
+                #print('parse article url:', article)
+                res.append(self._parse_article(article))
+                sleep(sleep_time)
+            self._output(board + str(start), res)
+            
+        #return res
+
+    def _pages(self, board, start, end):
+        for page_range in range(start, end + 1):
+            yield self.main_url + board + "/index" + str(page_range) + ".html"
+    
+    def _articles(self, page):
+        soup = BeautifulSoup(self._get_html(page), "lxml")
+        divs = soup.find_all('div', 'r-ent')
+        for div in divs:
+            if div.find('a'):
+                # 文章標題
+                # print(div.find('a').string)
+                yield div.find('a')['href']
+    
+    def _parse_article(self, url):
+        soup = BeautifulSoup(self._get_html(self.root_url + url), 'lxml')
+        try:
+            article = {}
+
+            # 取得文章作者與文章標題
+            article["Author"] = soup.select(".article-meta-value")[0].contents[0].split(" ")[0]
+            article["Title"] = re.sub('　', ' ', soup.select(".article-meta-value")[2].contents[0])
+            
+            # 取得內文
+            content = ""
+            for tag in soup.select("#main-content")[0]:
+                if type(tag) is NavigableString and tag != '\n':
+                    content += tag
+                    break
+            article["Content"] = content
+            
+            # 處理回文資訊
+            upvote = 0
+            downvote = 0
+            novote = 0
+            response_list = []
+
+            for response_struct in soup.select(".push"):
+
+                #跳脫「檔案過大！部分文章無法顯示」的 push class
+                if "warning-box" not in response_struct['class']:
+
+                    response_dic = {}
+                    response_dic["Content"] = response_struct.select(
+                        ".push-content")[0].contents[0][1:]
+                    response_dic["Vote"] = response_struct.select(
+                        ".push-tag")[0].contents[0][0]
+                    response_dic["User"] = response_struct.select(
+                        ".push-userid")[0].contents[0]
+                    response_list.append(response_dic)
+
+                    if response_dic["Vote"] == u"推":
+                        upvote += 1
+                    elif response_dic["Vote"] == u"噓":
+                        downvote += 1
+                    else:
+                        novote += 1
+
+            article["Responses"] = response_list
+            article["UpVote"] = upvote
+            article["DownVote"] = downvote
+            article["NoVote"] = novote
+
+        except Exception as e:
+            print(e)
+            print('*** 在分析{0}的時候發生錯誤 ***'.format(self.root_url + url))
+
+        return article
+    
+    def _output(self, filename, data):
+        with open(filename + ".json", mode='w', encoding='utf-8') as op:
+            op.write(json.dumps(data, indent=4, ensure_ascii=False))
+        
+    def _get_html(self, object):
+        return self.session.get(object).text
+
+if __name__ == "__main__":
+    ptt = ptt_scraper()
+    ptt.scraper()
+    #print('Done')
+    
