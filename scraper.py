a39886bc577e28646db7c9c0b791847faca50e13
zephyrxvxx7 <zephyrxvxx7@gmail.com>
2018-05-30 00:12:58 +0800

    Fix some text not to fetch bug.

--- scraper.py
+++ scraper.py
@@ -1,8 +1,10 @@
 import json
 import requests
+import argparse
 from time import sleep
 from bs4 import BeautifulSoup
 from bs4.element import NavigableString
+from six import u
 import re
 
 class ptt_scraper():
@@ -30,7 +32,7 @@ class ptt_scraper():
                 #print('parse article url:', article)
                 res.append(self._parse_article(article))
                 sleep(sleep_time)
-            self._output(board + str(start), res)
+            self._output(board + str(start) + "-" + str(end), res)
             
         #return res
 
@@ -48,7 +50,8 @@ class ptt_scraper():
                 yield div.find('a')['href']
     
     def _parse_article(self, url):
-        soup = BeautifulSoup(self._get_html(self.root_url + url), 'lxml')
+        soup = BeautifulSoup(self._get_html(
+            self.root_url + url), 'html.parser')
         try:
             article = {}
 
@@ -58,10 +61,29 @@ class ptt_scraper():
             
             # 取得內文
             content = ""
-            for tag in soup.select("#main-content")[0]:
-                if type(tag) is NavigableString and tag != '\n':
-                    content += tag
-                    break
+            main_content = soup.select("#main-content")[0]
+            for meta in main_content.select('div.article-metaline'):
+                meta.extract()
+            for meta in main_content.select('div.article-metaline-right'):
+                meta.extract()
+            pushes = soup.select(".push")
+            for push in main_content.find_all('div', class_='push'):
+                push.extract()
+            # 移除 '※ 發信站:' (starts with u'\u203b'), '◆ From:' (starts with u'\u25c6'), 空行及多餘空白
+            # 保留英數字, 中文及中文標點, 網址, 部分特殊符號
+            filtered = [v for v in main_content.stripped_strings if v[0]
+                        not in [u'※', u'◆'] and v[:2] not in [u'--']]
+            expr = re.compile(
+                u(r'[^\u4e00-\u9fa5\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b\s\w:/-_.?~%()]'))
+            for i in range(len(filtered)):
+                filtered[i] = re.sub(expr, '', filtered[i])
+
+            filtered = [_f for _f in filtered if _f]  # remove empty strings
+            # remove last line containing the url of the article
+            filtered = [x for x in filtered if url not in x]
+            filtered = [x for x in filtered if "http" not in x]
+            content = ' '.join(filtered)
+            content = re.sub(r'(\s)+', ' ', content)
             article["Content"] = content
             
             # 處理回文資訊
@@ -70,7 +92,7 @@ class ptt_scraper():
             novote = 0
             response_list = []
 
-            for response_struct in soup.select(".push"):
+            for response_struct in pushes:
 
                 #跳脫「檔案過大！部分文章無法顯示」的 push class
                 if "warning-box" not in response_struct['class']:
@@ -97,8 +119,8 @@ class ptt_scraper():
             article["NoVote"] = novote
 
         except Exception as e:
-            print(e)
             print('*** 在分析{0}的時候發生錯誤 ***'.format(self.root_url + url))
+            print(e)
 
         return article
     
@@ -110,7 +132,22 @@ class ptt_scraper():
         return self.session.get(object).text
 
 if __name__ == "__main__":
+    board = "Gossiping"
+    start = 38900
+    end = 38900
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+            A crawler for the web version of ptt.cc.
+            Input: board name and page indices
+            Output: BOARD_NAME-START_INDEX-END_INDEX.json
+        ''')
+    parser.add_argument('-b', metavar='BOARD_NAME',
+                        help='Board name', required=True)
+    parser.add_argument('-i', metavar=('START_INDEX', 'END_INDEX'),
+                        type=int, nargs=2, help="Start and end index", required=True)
+    parser.add_argument('-t', metavar='DELAY_TIME',
+                        type=float, help="Delay of time(sec.)", default=0.5)
+    args = parser.parse_args()
     ptt = ptt_scraper()
-    ptt.scraper()
-    #print('Done')
+    ptt.scraper(board=args.b, start=args.i[0], end=args.i[-1], sleep_time=args.t)
+    print('All mission is complete!')
     
